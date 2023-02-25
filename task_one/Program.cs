46baa47856758d8c8010afb58e6a2d21abab9d02
SIngleFon <64650782+SIngleFon@users.noreply.github.com>
2023-02-25 03:05:50 +0300

    Initial commit

---
+++ task_one/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int FirstNumber = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите второе число: ");
+int SecondNumber = int.Parse(Console.ReadLine() ?? "");
+int max = FirstNumber;
+if(FirstNumber > SecondNumber)
+{
+    Console.WriteLine($"Max = {FirstNumber}");
+}
+else
+{
+    Console.WriteLine($"Max = {SecondNumber}");
+}
